68fc8da15068dabc7c90a694d81c9a1309002327
Celasha <kdembner@gmail.com>
2026-09-10 14:50:57 -0700

    chore(main): release 2.107.7 (#705)
    * chore(main): release 2.107.7
    
    * chore: sync version and format release notes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -147,7 +147,7 @@ if (isCombatSimulatorPage()) {
     const toolashaRoot = targetWindow.Toolasha || {};
     targetWindow.Toolasha = toolashaRoot;
 
-    toolashaRoot.version = '2.107.6';
+    toolashaRoot.version = '2.107.7';
 
     toolashaRoot.features = {
         list: () => config.getFeaturesByCategory(),
