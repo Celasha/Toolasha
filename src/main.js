c0351e16210daccada9b4ed1d8adfc3bbc3ef1f2
Celasha <kdembner@gmail.com>
2026-09-21 11:26:04 -0700

    chore(main): release 2.110.1 (#719)
    * chore(main): release 2.110.1
    
    * chore: sync version and format release notes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -165,7 +165,7 @@ if (combatSimSite === 'shykai') {
     const toolashaRoot = targetWindow.Toolasha || {};
     targetWindow.Toolasha = toolashaRoot;
 
-    toolashaRoot.version = '2.110.0';
+    toolashaRoot.version = '2.110.1';
 
     toolashaRoot.features = {
         list: () => config.getFeaturesByCategory(),
